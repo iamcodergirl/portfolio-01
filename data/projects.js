d747ad8296bd4476f049c58ec6dcfa9f23897be2
iamcodergirl <iamcodergirl@gmail.com>
2025-01-28 21:06:11 +0200

    UPDATE021

--- data/projects.js
+++ data/projects.js
@@ -1,6 +1,6 @@
 const  projectData = [
     {
-        image: '/work/3.png?url=imageUrlHere',
+        image: './work/3.png?url=imageUrlHere',
         category: 'Portraits',
         date: 'February 11, 2024',
         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem recusandae vitae molestias quae laboriosam, illo velit cumque corrupti.',
@@ -8,7 +8,7 @@ const  projectData = [
         codepen: '/',
     },
     {
-        image: '/work/5.png?url=imageUrlHere',
+        image: './work/5.png?url=imageUrlHere',
         category: 'Models',
         date: 'April 22, 2023',
         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem recusandae vitae molestias quae laboriosam, illo velit cumque corrupti.',
@@ -16,7 +16,7 @@ const  projectData = [
         codepen: '/',
     },
     {
-        image: '/work/6.png?url=imageUrlHere',
+        image: './work/6.png?url=imageUrlHere',
         category: 'Models',
         date: 'Nov 05, 2023',
         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem recusandae vitae molestias quae laboriosam, illo velit cumque corrupti.',
@@ -24,7 +24,7 @@ const  projectData = [
         codepen: '/',
     },
     {
-        image: '/work/7.png?url=imageUrlHere',
+        image: './work/7.png?url=imageUrlHere',
         category: 'Corporate',
         date: 'Jan 14, 2021',
         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem recusandae vitae molestias quae laboriosam, illo velit cumque corrupti.',
@@ -32,7 +32,7 @@ const  projectData = [
         codepen: '/',
     },
     {
-        image: '/work/8.png?url=imageUrlHere',
+        image: './work/8.png?url=imageUrlHere',
         category: 'Fashion',
         date: 'Jan 31, 2023',
         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem recusandae vitae molestias quae laboriosam, illo velit cumque corrupti.',
@@ -40,7 +40,7 @@ const  projectData = [
         codepen: '/',
     },
     {
-        image: '/work/9.png?url=imageUrlHere',
+        image: './work/9.png?url=imageUrlHere',
         category: 'Fashion',
         date: 'Aug 31, 2022',
         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem recusandae vitae molestias quae laboriosam, illo velit cumque corrupti.',
@@ -48,7 +48,7 @@ const  projectData = [
         codepen: '/',
     },
     {
-        image: '/work/10.png?url=imageUrlHere',
+        image: './work/10.png?url=imageUrlHere',
         category: 'Corporate',
         date: 'Mar 11, 2013',
         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem recusandae vitae molestias quae laboriosam, illo velit cumque corrupti.',
@@ -56,7 +56,7 @@ const  projectData = [
         codepen: '/',
     },
     {
-        image: '/work/3.png?url=imageUrlHere',
+        image: './work/3.png?url=imageUrlHere',
         category: 'Models',
         date: 'Oct 23, 2019',
         description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dolorem recusandae vitae molestias quae laboriosam, illo velit cumque corrupti.',
